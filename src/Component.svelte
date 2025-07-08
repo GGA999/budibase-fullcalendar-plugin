7b2915042d5b609ae431b0b37a0d905176cdf80e
George Alin Gilea <ggilea@sorint.com>
2025-07-08 16:58:35 +0200

    Test f

--- src/Component.svelte
+++ src/Component.svelte
@@ -17,68 +17,72 @@
   export let dataProvider;
   export let mappingColor;
   export let allday;
-
-  // 🔥 Nuovo mapping dinamico dei colori
   export let typeColorMapping = [];
-
-  let eventsList = [];
-
-  function generateColorMap() {
-    return typeColorMapping.reduce((acc, item) => {
-      acc[item.type] = item.color;
-      return acc;
-    }, {});
-  }
-
-  function buildEvents() {
-    const colorMap = generateColorMap();
-    eventsList = [];
-
-    if (dataProvider?.rows) {
-      dataProvider.rows.forEach(event => {
-        const eventType = event.type;
-        const eventColor = colorMap[eventType] || mappingColor || '#313131';
-
-        eventsList.push({
-          title: event[mappingTitle],
-          date: event[mappingDate],
-          start: event[mappingStart],
-          end: event[mappingEnd],
-          color: eventColor,
-          event,
-          allDay: allday,
-        });
-      });
-    }
-  }
-
-  onMount(buildEvents);
-
-  // 🔄 Quando cambiano i dati, aggiorna la lista eventi
-  $: dataProvider, typeColorMapping, mappingColor, buildEvents();
-
-  let options = {
-    headerToolbar: {
-      start: headerOptionsStart,
-      center: headerOptionsCenter,
-      end: headerOptionsEnd,
-    },
-    plugins: [daygridPlugin, listPlugin, timeGridPlugin],
-    initialDate: Date.now(),
-    locale: language,
-    dayMaxEvents: true,
-    eventClick: (event) => {
-      calendarEvent({ value: event.event });
-    },
-    events: eventsList,
-    eventColor: '#378006',
-    theme: true,
-    ...langs[codeLang(language)],
-  };
 
   export let headerOptionsStart;
   export let headerOptionsCenter;
   export let headerOptionsEnd;
+
+  let eventsList = [];
+
+  function generateColorMap() {
+    const map = {};
+    (typeColorMapping || []).forEach(item => {
+      if (item?.type && item?.color) {
+        map[item.type] = item.color;
+      }
+    });
+    return map;
+  }
+
+  function buildEvents() {
+    const colorMap = generateColorMap();
+    const newEvents = [];
+
+    if (dataProvider?.rows) {
+      dataProvider.rows.forEach(event => {
+        const type = event.type;
+        const eventColor = colorMap[type] || mappingColor || '#313131';
+        newEvents.push({
+          title: event[mappingTitle],
+          start: event[mappingStart] || event[mappingDate],
+          end: event[mappingEnd],
+          color: eventColor,
+          allDay: allday,
+          event
+        });
+      });
+    }
+
+    eventsList = newEvents;
+  }
+
+  // Costruisce gli eventi all'inizio
+  onMount(() => {
+    buildEvents();
+  });
+
+  // Ricostruisce quando cambia il dataProvider o la mappatura dei colori
+  $: if (dataProvider || typeColorMapping) {
+    buildEvents();
+  }
+
+  let options;
+  $: options = {
+    headerToolbar: {
+      start: headerOptionsStart,
+      center: headerOptionsCenter,
+      end: headerOptionsEnd
+    },
+    plugins: [daygridPlugin, timeGridPlugin, listPlugin],
+    initialDate: new Date().toISOString().substring(0, 10),
+    locale: language,
+    dayMaxEvents: true,
+    eventClick: info => calendarEvent({ value: info.event.extendedProps.event }),
+    events: eventsList,
+    theme: true,
+    ...langs[codeLang(language)]
+  };
 
   const { styleable } = getContext("sdk");
   const component = getContext("component");
